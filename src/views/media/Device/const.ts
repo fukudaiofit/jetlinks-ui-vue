f9cb1374745bbd185f5447afbcab364227b27550
leiqiaochu <leiqiaochu@aossci.com>
2023-07-03 17:38:58 +0800

    feat: 视频中心国际化补充

--- src/views/media/Device/const.ts
+++ src/views/media/Device/const.ts
@@ -2,7 +2,7 @@ import createI18n from '@/locales/index';
 const { t } = createI18n.global
 export const PROVIDER_OPTIONS = [
     { label: 'GB/T28181', value: 'gb28181-2016' },
-    { label: t('media.Device.cosnt.fixed'), value: 'fixed-media' },
+    { label: t('media.Device.const.fixed'), value: 'fixed-media' },
 ]
 export const streamMode = [
     { label: 'UDP', value: 'UDP' },
